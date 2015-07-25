e5b1118bac8b647c1330bb9fa4ad0d763e5d7bb2
Sam Halliday <sam.halliday@gmail.com>
2015-07-25 11:05:44 +0100

    Q3

---
+++ 3.el
@@ -0,0 +1,29 @@
+;;; 3.el --- Problem 3 of 99
+;;; Commentary:
+;;
+;;  Find the K'th element of a list.
+;;  The first element in the list is number 1.
+;;  (element-at '(a b c d e) 3)\\
+;;  C
+;;
+;;; Code:
+
+(require 'ert)
+
+(defun element-at (els k)
+  "Element of ELS at position K (1-based indexing)."
+  (if (<= k 1)
+      (car els)
+    (element-at (cdr els) (1- k))))
+
+(ert-deftest ert-test-3 ()
+  (should (equal 'c (element-at '(a b c d e) 3)))
+
+  (should (equal 'a (element-at '(a b c d e) 1)))
+  (should (equal 'a (element-at '(a) 1)))
+
+  (should (equal nil (element-at '(a b c d e) 6)))
+  (should (equal 'a (element-at '(a b c d e) 0)))
+)
+
+;;; 3.el ends here
