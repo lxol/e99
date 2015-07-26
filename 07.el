6e5791dcc2dbd7333145762b0c2aae55f7b4226a
Sam Halliday <sam.halliday@gmail.com>
2015-07-26 20:14:49 +0100

    answer Q7

--- 07.el
+++ 07.el
@@ -15,10 +15,22 @@
 
 (defun my-flatten (lst)
   "A flattened version of LST."
-  )
+  (let ((el nil)
+        (flat nil))
+    (while (not (null lst))
+      (setq el (car lst)
+            flat (append flat (if (listp el)
+                                  (my-flatten el)
+                                (list el)))
+            lst (cdr lst)))
+    flat))
 
 (ert-deftest Q07 ()
+  (should (equal '(a) (my-flatten '(a))))
+  (should (equal '(a b) (my-flatten '(a b))))
   (should (equal '(a b c d e) (my-flatten '(a (b (c d) e)))))
+  (should (equal '(a b c d e) (my-flatten '((((((a b c d e)))))))))
+  (should (equal '(a b c) (my-flatten '((a b c) (nil) nil))))
   )
 
 ;;; 07.el ends here
