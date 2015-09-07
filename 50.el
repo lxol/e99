c265165a9605eab7f7df1e383481db2b07198f10
Sam Halliday <sam.halliday@gmail.com>
2015-09-07 16:28:58 +0100

    huffman attempts

--- 50.el
+++ 50.el
@@ -50,14 +50,59 @@
 ;;
 ;;; Code:
 
+(defun huffman-tree (freqs)
+  "Huffman tree for FREQS `((sym count) ...)'.
+
+Nodes can be either:
+
+* leaf `(sym count)'
+* internal `(left right count)' (i.e. count is cached)
+
+where left and right are nodes.  The Huffman tree is an internal node."
+  (let ((q1 (sort freqs (lambda (a b) (< (cadr a) (cadr b)))))
+        (q2))
+    (cl-flet* ((weight (node) (pcase node
+                                ;; TODO how to match and discard syms?
+                                (`(,sym ,count)         count)
+                                (`(,left ,right ,count) count)
+                                (_                      0)))
+               (nlowest () (let ((left  (weight (car q1)))
+                                 (right (weight (car q2))))
+                             (if (<= left right)
+                                 (pop q2)
+                               (pop q1)))))
+      (while (and (< 1 (length (append q1 q2))))
+        (let ((left (nlowest))
+              (right (nlowest)))
+          ;; TODO function to push-to-end
+          (setq q2 (append q2
+                           `((,left ,right ,(+ (weight left) (weight right))))))))
+      q2)))
+
+(defun huffman-table (tree)
+  "Huffman table `((sym code) ...)' for a `huffman-tree' TREE."
+  (pcase node
+    (`(,sym ,count)  prefix)
+    (`(,left ,right) (concat (prefix "0" (encode left))
+                             (prefix "1" (encode right))))))
+
 (defun huffman (freqs)
   "Huffman table `((sym code) ...)' for FREQS `((sym count) ...)'."
-  (let ((sorted (sort freqs (lambda (a b) (< (cadr a) (cadr b))))))
-    sorted
-    )
-  )
+  (huffman-table (huffman-tree freqs)))
 
 (ert-deftest Q50 ()
+  (should (equal '(((f 5) (e 9) 14))
+                 (huffman-tree '((e 9) (f 5)))))
+
+  (should (equal '(((f 5) (e 9) 14))
+                 (huffman-tree '((e 9) (f 5)))))
+
+  ;; TODO: check if this is actually correct
+  (should (equal '(((((((f 5) (e 9) 14) (c 12) 26) (b 13) 39) (d 16) 55) (a 45) 100))
+                 (huffman-tree '((a 45) (b 13) (c 12) (d 16) (e 9) (f 5)))))
+
+  ;; TODO tests around huffman-table
+
   (should (equal '((a "0") (b "101") (c "100") (d "111") (e "1101") (f "1100"))
                  (huffman '((a 45) (b 13) (c 12) (d 16) (e 9) (f 5)))))
   )
